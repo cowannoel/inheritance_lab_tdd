74bbc461809b79e56cee9744b8c85fd431c3a4b1
cowannoel <cowannoel@gmail.com>
2020-03-30 21:46:21 +0100

    initial commit employee and methods done

---
+++ src/main/java/Employee.java
@@ -0,0 +1,34 @@
+public abstract class Employee {
+
+    private String name;
+    private String NI;
+    private double salary;
+
+    public Employee (String name, String NI, double salary) {
+        this.name = name;
+        this.NI = NI;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNI() {
+        return NI;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void raiseSalary(double amount){
+        if(amount < 0){
+            this.salary += amount;
+        }
+    }
+
+    public double payBonus(){
+        return this.salary / 100
+    }
+}
